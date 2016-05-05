5b1c8053f5c52666ed58a521fd826d0515ed0d18
Sahba Bahizad <sarahjune919@gmail.com>
2016-05-05 09:10:43 -0700

    change typos plural collection

--- Bookmarks/server/main.js
+++ Bookmarks/server/main.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
-import { bookmarksCollection from '../collection/collection.js'}
-import {bookmarksDummyData  from '../collection/collection.js'}
+import { bookmarksCollection from '../collections/collections.js'}
+import {bookmarksDummyData  from '../collections/collections.js'}
 Meteor.startup(() => {
   //remove any database values that are present
   bookmarksCollection.remove({}); //delete all records ( this will only work on server side)
